83d43c495e4bfbbc929ec8f2d0d8bb6dc7bd90c8
Xailus <Chrisseiler96@gmail.com>
2019-03-06 16:48:10 -0500

    Day 2 material complete

--- whotweetedit/app.py
+++ whotweetedit/app.py
@@ -2,21 +2,28 @@
 Main Application and routing logic for WhoTweetedIt
 """
 
-
+from decouple import config
 from flask import Flask, render_template, request
-from .models import DB
+from .models import DB, User
 
 
 def create_app():
 	""" Create and configure an instance of the Flask application."""
 	app = Flask(__name__)
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+	app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	app.config['ENV'] = config('ENV')
 	DB.init_app(app)
 
 	@app.route('/')
 	def root():
-		return render_template('base.html')
+		users = User.query.all()
+		return render_template('base.html', title = 'Home', users=users)
 
-
+	@app.route('/reset')
+	def reset():
+		DB.drop_all()
+		DB.create_all()
+		return render_template('base.html', title='DB Reset!', users=[])
 
 	return app
