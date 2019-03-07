82a5dcb66ac4c9310fe18339ef148770b13ededc
Xailus <Chrisseiler96@gmail.com>
2019-03-07 01:02:13 -0500

    Finished Day 3 code

--- whotweetedit/app.py
+++ whotweetedit/app.py
@@ -5,20 +5,36 @@ Main Application and routing logic for WhoTweetedIt
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
-
+from .twitter import add_or_update_user
 
 def create_app():
 	""" Create and configure an instance of the Flask application."""
 	app = Flask(__name__)
 	app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-	app.config['ENV'] = config('ENV')
 	DB.init_app(app)
+
 
 	@app.route('/')
 	def root():
 		users = User.query.all()
 		return render_template('base.html', title = 'Home', users=users)
+	
+	@app.route('/user', methods=['POST'])
+	@app.route('/user/<name>',methods=['GET'])
+	def user(name=None, message=''):
+		name = name or request.values['user_name']
+		try:
+			if request.method == 'POST':
+				add_or_update_user(name)
+				message = 'User {} successfully added!'.format(name)
+			tweets = User.query.filter(User.name == name).one().tweets
+
+		except Exception as e:
+			message = 'Error adding {}: {}'.format(name,e)
+			tweets=[]
+
+		return render_template('user.html', title=name, tweets=tweets, message=message)
 
 	@app.route('/reset')
 	def reset():
